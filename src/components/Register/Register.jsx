929b02523cbd665532e4a77a6daff5b8f8ce72b9
konstantin <kot_sky@mail.ru>
2022-07-19 22:07:33 +0600

    вёрстка 10процентов

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,12 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import './Register.css';
+import LogoLink from '../LogoLink/LogoLink.jsx';
+import ErrorMessage from '../ErrorMessage/ErrorMessage.jsx';
 
 function Register() {
   return (
     <section className="register">
       <div className="register__container">
-
+        <div className="register__linkToMain">
+          <LogoLink />
+        </div>
+        <h2 className="register__title">
+          Добро пожаловать!
+        </h2>
+        <form className="register__form">
+          <div className="register__inputsBlock">
+            <label>
+              <span className="register__formLabelText">Имя</span>
+              <input className="register__input" type="text" name="name" placeholder="Имя"/>
+            </label>
+            <label>
+              <span className="register__formLabelText">E&#8209;mail</span>
+              <input className="register__input" type="email" name="email" placeholder="E-mail"/>
+            </label>
+            <label>
+              <span className="register__formLabelText">Пароль</span>
+              <input className="register__input register__input_type_password" type="password" name="password" placeholder="Пароль"/>
+              <ErrorMessage />
+            </label>
+          </div>
+          <div className="register__buttonsBlock">
+            <button className="register__buttonSubmit">Зарегистрироваться</button>
+            <Link to="/signin" className="register__link">
+              Уже зарегистрированы?
+              <span className="register__linkText">Войти</span>
+            </Link>
+          </div>
+        </form>
       </div>
     </section>
   );
